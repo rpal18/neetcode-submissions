31cd1cd9466ca38019d9fae5f52eac4225177dfb
ROHIT PAL <168446325+rpal18@users.noreply.github.com>
2026-08-02 12:02:14 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int farthest = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            if(i > farthest){
+                return false;
+            }
+            
+            if(farthest >= n - 1){
+                return true;
+            }
+
+            farthest = Math.max(farthest , i + nums[i]);
+
+        }
+        return true;
+    }
+}
